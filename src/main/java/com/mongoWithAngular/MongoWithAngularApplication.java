e6222e1de9c0e8f73e9b48e7099b235d0f5202d0
zorik zaqaryan <zorikz@energizeglobal.com>
2016-05-30 17:53:57 +0400

    initial commit comment

---
+++ src/main/java/com/mongoWithAngular/MongoWithAngularApplication.java
@@ -0,0 +1,12 @@
+package com.mongoWithAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWithAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoWithAngularApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mongoWithAngular/MongoWithAngularApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mongoWithAngular;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MongoWithAngularApplication.class)
+@WebAppConfiguration
+public class MongoWithAngularApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
